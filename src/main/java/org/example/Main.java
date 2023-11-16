55e7740df22afebe7d292b282264829b78244752
miksor2023 <miksor@yandex.ru>
2023-11-16 16:01:01 +0300

    HW

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,42 @@
 package org.example;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            chekAuthorizationData("werR%___", "kjhgyguy", "kjhgyguy");
+        } catch (WrongLoginException exception){
+            System.out.println(exception);
+        } catch (WrongPasswordException exception){
+            System.out.println(exception);
+        }
     }
+    public static void chekAuthorizationData (String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
+        if (!checkString(login) || login.isEmpty()){
+            throw new WrongLoginException();
+        }
+        if (!checkString(password) || !password.equals(confirmPassword) || password.isEmpty()){
+            throw new WrongPasswordException();
+        }
+    }
+    public static boolean checkString(String string){// проверка строки: true - если удовлетворяет условиям
+        if (string.length() > 20) {
+            return false;
+        }
+        String allowedSymbols = "0123456789abcdefghijklmnoprstuvwxyz_";
+        string = string.toLowerCase();
+        for (int i = 0; i < string.length(); i++) {
+            Boolean forbiddenSymbol = true;
+            for (int j = 0; j < allowedSymbols.length(); j++) {
+                if(string.charAt(i) == allowedSymbols.charAt(j)){
+                    forbiddenSymbol = false;
+                }
+            }
+            if(forbiddenSymbol){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
---
+++ src/main/java/org/example/WrongPasswordException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class WrongPasswordException extends Exception{
+    public WrongPasswordException(){
+        super();
+    }
+}
